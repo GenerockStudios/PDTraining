f301624c1ab88a09873ac99015b8a04438bbd079
Erock - Generock Studios <120332380+GenerockStudios@users.noreply.github.com>
2023-09-10 16:49:43 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+    if (args.Length > 0)
+    {
+        foreach (string line in args)
+        {
+            Console.WriteLine("---------------------------------------------------------------");
+            Console.WriteLine(line);
+            Console.WriteLine("---------------------------------------------------------------");
+            Read(line);
+        }
+    }
+
+Console.ReadKey();
+
+void Read(string path)
+{
+    string[] lines = File.ReadAllLines(path);
+
+    foreach (string line in lines)
+    {
+        Console.WriteLine(line);
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# PDTraining
+
+Petit projet en lien avec le tutoriel présent sur Youtube
+cela aide à comprendre comment fonctionne les points d'entrée
+et les arguments
+
+## Lien de la vidéo youtube https://youtu.be/iY4UudSFdKY
